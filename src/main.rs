0b1a9fef8ff205a694f681f14ddbd9d3a364950f
LiuZhen <liuzhen@51hitech.com>
2023-06-26 11:27:38 +0800

    添加法线着色
    enum for hitable
    if-else to match
    update hierarchy of the lal package

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use std::io::{ErrorKind, Write};
 pub mod lal;
 pub mod ray;
 
+
+enum Hit<T>{
+    Yes(T),
+    No,
+}
+
+
 fn write_to(fileName : &str, bytes : &[u8]) -> std::io::Result<()>
 {
     let mut ppm_file = File::open(fileName).unwrap_or_else(|error| {
@@ -21,36 +28,49 @@ fn write_to(fileName : &str, bytes : &[u8]) -> std::io::Result<()>
     ppm_file.flush()?;
     Ok(())
 }
-
-fn hit_sphere(center : lal::vec3, radius : f64, r : &ray::ray_basic) -> bool
+// p(t) = A + t*B.
+//t*t*dot(B,B) + 2*t*dot(A-C,A-C) + dot(C,C) - R*R = 0
+fn hit_sphere(center : lal::vector::vec3, radius : f64, r : &ray::ray_basic) -> Hit<f64>
 {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
-}
-
-
-fn color(r : &ray::ray_basic) -> lal::vec3{
-    let a = hit_sphere(lal::vec3::create(0.0, 0.0, -1.0), 0.5, r);
-    if a {
-        return lal::vec3::create(1.0, 0.0, 0.0);
+    //println!("{}", discriminant);
+    if discriminant < 0.0{
+        //-1.0
+        Hit::No
     }
     else {
-        let unit_direction = lal::vec3::unit_vector(r.direction());
-        let t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * lal::vec3::create(1.0, 1.0, 1.0) + t * lal::vec3::create(0.5, 0.7, 1.0);
+        //println!("hello");
+        Hit::Yes((-b - discriminant.sqrt()) / (2.0 * a))
+    }
+}
+
+
+fn color(r : &ray::ray_basic) -> lal::vector::vec3{
+    let t = hit_sphere(lal::vector::vec3::create(0.0, 0.0, -1.0), 0.5, r);
+
+    match t {
+        Hit::Yes(v) => {
+            let N = lal::vector::vec3::unit_vector(r.point_at_parameter(v) - lal::vector::vec3::create(0.0,0.0,-1.0));
+            0.5 * lal::vector::vec3::create(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0)
+        },
+        Hit::No =>{
+            let unit_direction = lal::vector::vec3::unit_vector(r.direction());
+            let t = 0.5 * (unit_direction.y() + 1.0);
+            (1.0 - t) * lal::vector::vec3::create(1.0, 1.0, 1.0) + t * lal::vector::vec3::create(0.5, 0.7, 1.0)
+        }
     }
 
 
 }
 
-// fn color(r : ray::ray_basic) -> lal::vec3{
-//     let unit_direction = lal::vec3::unit_vector(r.direction());
+// fn color(r : ray::ray_basic) -> lal::vector::vec3{
+//     let unit_direction = lal::vector::vec3::unit_vector(r.direction());
 //     let t = 0.5 * (unit_direction.y() + 1.0);
-//     lal::vec3::create(1.0, 1.0, 1.0) * (1.0 - t) + lal::vec3::create(0.5, 0.7, 1.0) * t
+//     lal::vector::vec3::create(1.0, 1.0, 1.0) * (1.0 - t) + lal::vector::vec3::create(0.5, 0.7, 1.0) * t
 // }
 
 
@@ -65,22 +85,36 @@ fn create_pixel_buffer(width : u32, height : u32) -> String
     temp.push_str(&height.to_string());
     temp.push_str("\n255\n");
 
-    let lower_left_corner = lal::vec3::create(-2.0, -1.0, -1.0);
-    let horizontal = lal::vec3::create(4.0, 0.0, 0.0);
-    let vertical = lal::vec3::create(0.0, 2.0, 0.0);
-    let origin = lal::vec3::create(0.0, 0.0, 0.0);
+    let lower_left_corner = lal::vector::vec3::create(-2.0, -1.0, -1.0);
+    let horizontal = lal::vector::vec3::create(4.0, 0.0, 0.0);
+    let vertical = lal::vector::vec3::create(0.0, 2.0, 0.0);
+    let origin = lal::vector::vec3::create(0.0, 0.0, 0.0);
 
     for y in 0..height{
         for x in 0..width{
             // let r : f32 = x as f32 / width as f32;
             // let g : f32 = (height - y) as f32 / height as f32;
             // let b : f32 = 0.0;
-            // let col = lal::vec3::create(
+            // let col = lal::vector::vec3::create(
             //     x as f64 / width as f64, 
             //     (height - y) as f64 / height as f64,
             //     0.0);
-            let ray_temp = ray::ray_basic::create(origin, 
-                lower_left_corner + horizontal * (x as f64) + vertical * ((height - y) as f64));
+
+
+
+            let u =  x as f64 / width as f64;
+            let v = (height - y) as f64 / height as f64;
+
+
+            let ray_temp =  ray::ray_basic::create(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical
+            );
+
+            // let color_result = lal::vector::vec3::create(
+            //     x as f64 / width as f64, 
+            //     (height - y) as f64 / height as f64,
+            //     0.0);
 
             let color_result = color(&ray_temp);
 
@@ -104,12 +138,9 @@ fn main() {
     let num_x = 200;
     let num_y = 100;
 
-    let mut test_aaa = lal::vec3::create(11.11, 30.5, 11.11);
-    let aaa = 3.14;
+    // let test_bbb = lal::vector::vec3::create(23.11, 34.656, 24.989);
 
-    // let test_bbb = lal::vec3::create(23.11, 34.656, 24.989);
-
-    // let unit_vector_test = lal::vec3::unit_vector(test_aaa);
+    // let unit_vector_test = lal::vector::vec3::unit_vector(test_aaa);
 
     // println!("cross result is {:#?}", unit_vector_test);
 
--- src/ray.rs
+++ src/ray.rs
@@ -3,26 +3,26 @@ use crate::lal;
 
 pub struct ray_basic
 {
-    A : lal::vec3,
-    B : lal::vec3,
+    A : lal::vector::vec3,
+    B : lal::vector::vec3,
 }
 
 impl ray_basic{
-    pub fn create(a : lal::vec3, b : lal::vec3) -> Self{
+    pub fn create(a : lal::vector::vec3, b : lal::vector::vec3) -> Self{
         Self {
             A: a,
             B: b,
         }
     }
-    pub fn origin(&self) -> lal::vec3{
+    pub fn origin(&self) -> lal::vector::vec3{
         self.A
     }
 
-    pub fn direction(&self) -> lal::vec3{
+    pub fn direction(&self) -> lal::vector::vec3{
         self.B
     }
 
-    pub fn point_at_parameter(&self, t : f64) -> lal::vec3{
+    pub fn point_at_parameter(&self, t : f64) -> lal::vector::vec3{
         self.A + self.B * t
     }
 }
